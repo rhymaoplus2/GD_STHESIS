117f641bea12f42204d7d2745c7afb1505abccdb
Reyris Milmao <reyrismilmao@gmail.com>
2023-01-13 15:39:17 +0800

    records change to add file name

--- Thesis/teacher/records/add.php
+++ Thesis/teacher/records/add.php
@@ -1,11 +1,7 @@
-
-
-
-<?php include './php/admin_update.php'; ?>
-
-
-
-
+<?php 
+   session_start();
+   include "../php/db_conn.php";
+   if (isset($_SESSION['username']) && isset($_SESSION['id'])) {   ?>
 
 
 <!DOCTYPE html>
@@ -14,11 +10,11 @@
 	<title>Update</title>
   <link  href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <style>
+  
 
-<style>
 
-
-.container {
+  .container {
 	min-height: 100vh;
 	display: flex;
 	justify-content: center;
@@ -152,307 +148,176 @@ font-size: 10px;;
   .sticky + .content {
     padding-top: 102px;
   }
-
   </style>
-
 </head>
 <body>
 
-
-
 <div class="header" id="myHeader">
 <?PHP include_once('header.php');?>
+</div>
+
+</div>
+<div class="container">
+      
+
+<!--  CRUD AREA -->
+<br>
+<br>
+
+
+  
+<form action="./php/add.php"
+      method="post">
+
+      <?php if (isset($_GET['success'])) { ?>
+           <div class="alert alert-success" role="alert">
+			  <?php echo $_GET['success']; ?>
+		    </div>
+		    <?php } ?>
+
+      
+  <h4 class="display-10 text-center" >Add Records</h4>
+
+
+ <?php if (isset($_GET['error'])) { 
+  include "./php/db_conn.php";
+  ?>
+  
+
+  <div class="alert alert-danger" role="alert">
+  <?php echo $_GET['error']; ?>
+</div>
+<?php } ?>
+
+
+
+
+
+<div class="mb-3">
+ <label for="exampleInputEmail1" class="form-label">Subject Name</label>
+ <select name="subjectname" id="subjectname" class="form-control">
+ <div <?php if (isset($name_error)): ?> class="form_error" <?php endif ?> >
+    <?php 
+    $query ="SELECT subjectname FROM subjects";
+  
+    $result = $conn->query($query);
+ 
+   
+    if($result->num_rows> 0){
+        while($optionData=$result->fetch_assoc()){
+        
+           
+          $option = $optionData['subjectname'];
+        
+    ?>
+    <?php
+    //selected option
+    if(!empty($id) && $id== $option){
+    // selected option
+    ?>
+    <option value="<?php echo $option; ?>" selected><?php echo $option; ?> </option>
+    <?php 
+continue;
+   }?>
+    <option value="<?php echo $option; ?>" ><?php echo $option; ?> </option>
+   <?php
+    }}
+    ?>
+</select>
+
+
+
+<br>
+
+<div class="mb-3">
+ <label for="exampleInputEmail1" class="form-label">Student ID </label>
+ <select name="studentid" id="studentid" class="form-control">
+  
+    <?php 
+   //$duplicate = "SELECT * FROM grade";    
+   //$dp = mysqli_query($conn,$duplicate);
+   $query ="SELECT id FROM students";
+   $result = $conn->query($query);
+    $d = $studentid;
+    if($result->num_rows> 0){
+        while($optionData=$result->fetch_assoc()){
+       
+            $option =$optionData['id'];
+
+    ?>
+    <?php
+    //selected option
+    if(!empty($id) && $id== $option){
+    // selected option
+    ?>
+    <option value="<?php echo $option; ?>" selected><?php echo $option; ?> </option>
+    <?php 
+continue;
+   }?>
+    <option value="<?php echo $option; ?>" ><?php echo $option; ?> </option>
+   <?php
+    }}
+    ?>
+</select>
+
+
+<br>
+
+<div class="form-group">
+    <label for="exampleInputEmail1" class="form-label">Grade</label>
+    <input 
+    type="text" 
+    class="form-control" 
+    id="grade" 
+    placeholder="ex:92"
+    name="grade">
+  </div>
+
+
+<br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <button type="submit" 
+          class="btn btn-primary"
+          name="create">Create</button>
+
+          <a class="link-primary" href="subjectlist.php">
+          <button type="button" class="btn btn-dark">
+
+      
+          Cancel
+
+          </button>
+          </a>
+</form>
 </div>
 
 
 
 
 
-<br>
-<br>
-	<div class="container">
-		<form action="php/update.php" 
-		      method="post">
-            
-		   <h4 class="display-4 text-center">Update</h4><hr><br>
-		   <?php if (isset($_GET['error'])) { ?>
-		   <div class="alert alert-danger" role="alert">
-			  <?php echo $_GET['error']; ?>
-		    </div>
-		   <?php } ?>
 
 
-<div class="form-group">
-    <label for="" class="form-label">LRN no.</label>
-    <input 
-	value="<?=$row['lrnnumber'] ?>" 
 
-    type="text" 
-    class="form-control" 
-    id="lrnnumber" 
-    placeholder="ex: 12913937"
-    name="lrnnumber">
-  </div>
-
-
-<!--
-  <div class="form-group">
- 
-
-    <label for="" class="form-label">Adviser ID</label>
-    <input 
-	value="<?=$row['adviser_id'] ?>" 
-    type="text" 
-    class="form-control" 
-    id="adviser_id" 
-    placeholder="ex: 12913937"
-    name="adviser_id">
-  </div>
-       -->
-
-
-
-
-
-
-
-
- 
-       <div class="mb-3">
-    <input
-    type="hidden"
-    class="form-control" 
-    id="adviser_id" 
-    placeholder="ex: Science"
-    name="adviser_id"
-    value="<?php echo $_SESSION['id']?>"
-    >
-    
-  </div>
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <div class="form-group">
-    <label for="exampleInputEmail1" class="form-label">First Name</label>
-    <input 
-	value="<?=$row['firstname'] ?>" 
-
-    type="text" 
-    class="form-control" 
-    id="firstname" 
-    placeholder="ex: Reyris"
-    name="firstname">
-  </div>
-
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Middle Name</label>
-    <input 
-	value="<?=$row['middlename'] ?>" 
-
-    type="middlename" 
-    class="form-control" 
-    id="middlename" 
-    placeholder="ex: Perolino"
-    name="middlename">
-
-  </div>
-
-  
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Last Name</label>
-    <input
-	value="<?=$row['lastname'] ?>" 
-	
-    type="lastname" 
-    class="form-control" 
-    id="lastname" 
-    placeholder="ex: Milmao"
-    name="lastname">
-
-  </div>
-
-
-
-  <div class="from-group mb-3">
-                                <label for="">Suffix</label>
-                                <select name="suffix" id="suffix" class="form-control"
-	value="<?=$row['suffix'] ?>" 
-								>
-                                    <option value=" ">None</option>
-                                    <option value="Jr.">Jr.</option>
-                                    <option value="Sr.">Sr.</option>
-                                </select>
-                            </div>
-
-
-
-                       <div class="from-group mb-3">
-                                <label for="">Gender</label>
-                                <select name="gender" id="gender" class="form-control" 
-	value="<?=$row['gender'] ?>" 
-								>
-                                    <option value="Male">Male</option>
-                                    <option value="Female">Female</option>
-                                </select>
-                            </div>
-
-
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Birth Place</label>
-    <input 
-	value="<?=$row['birthplace'] ?>" 
-
-    type="text" 
-    class="form-control" 
-    id="birthplace" 
-    placeholder="ex: Sto. Nino, Kolambugan Lanao del Norte"
-    name="birthplace">
-
-  </div>
-  
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Birth Date</label>
-    <input 
-	value="<?=$row['birthday'] ?>" 
-
-    type="date" 
-    class="form-control" 
-    id="birthday" 
-    placeholder="ex: August 17 2000"
-    name="birthday">
-
-  </div>
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Age</label>
-    <input 
-	value="<?=$row['age'] ?>" 
-
-    type="age" 
-    class="form-control" 
-    id="age" 
-    placeholder="ex: 22"
-    name="age">
-
-  </div>
-
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Address</label>
-    <input 
-	value="<?=$row['address'] ?>" 
-    type="address" 
-    class="form-control" 
-    id="address" 
-    placeholder="ex: Sto. Nino, Kolambugan Lanao del Norte"
-    name="address">
-
-  </div>
-
-
-  
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Parent/Guardian</label>
-    <input 
-	value="<?=$row['parent'] ?>" 
-    type="parent" 
-    class="form-control" 
-    id="parent" 
-    placeholder="ex: Reynato Milmao"
-    name="parent">
-
-  </div>
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">School Year</label>
-    <input 
-	value="<?=$row['schoolyear'] ?>" 
-    type="" 
-    class="form-control" 
-    id="schoolyear" 
-    placeholder="ex: 2022-2023"
-    name="schoolyear">
-
-  </div>
-
-
-  
-  <div class="from-group mb-3">
-                                <label for="">Year Level</label>
-                                <select name="grade" id="grade" class="form-control" 
-	value="<?=$row['grade'] ?>" 
-								>
-                <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                </select>
-                            </div>
-
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Section Name</label>
-    <input 
-	value="<?=$row['section'] ?>" 
-    type="" 
-    class="form-control" 
-    id="section" 
-    placeholder="ex: 2022-2023"
-    name="section">
-
-  </div>
-
-
-
-
-
-		   
-	
-
-
-		   
-		   <input type="text" 
-		          name="id"
-		          value="<?=$row['id']?>"
-		          hidden >
-		
-		   <button type="submit" 
-		           class="btn btn-primary"
-		           name="update">Update</button>
-
-
-				   <a class="link-primary" href="teacher_read.php" display-40>
-          <button type="button" class="btn btn-dark">
-
-      Cancel
-
-          </button>
-
-
-	
-	    </form>
-	</div>
-<br>
-<br>
-  <script>
+<script>
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
@@ -466,8 +331,18 @@ function myFunction() {
   }
 }
 </script>
-  
-  
+
+
+
+
+
+
+
+
+
 
 </body>
 </html>
+<?php }else{
+	header("Location:create.php");
+} ?>
--- Thesis/teacher/records/php/add.php
+++ Thesis/teacher/records/php/add.php
@@ -1,8 +1,7 @@
 <?php 
 
-if (isset($_GET['id'])) {
+if (isset($_POST['create'])) {
 	include "db_conn.php";
-
 	function validate($data){
         $data = trim($data);
         $data = stripslashes($data);
@@ -10,109 +9,45 @@ if (isset($_GET['id'])) {
         return $data;
 	}
 
-	$id = validate($_GET['id']);
+	$subjectname = validate($_POST['subjectname']);
+	$studentid = validate($_POST['studentid']);
+	$grade = validate($_POST['grade']);
+	$studentid = $_POST['studentid'];
+	$duplicate=mysqli_query($conn,"select * from grade where studentid='$studentid'");
+	$user_data =
 
-	$sql = "SELECT * FROM students WHERE id=$id";
-    $result = mysqli_query($conn, $sql);
+	'subjectname='.$subjectname.
+	'studentid='.$studentid.
+	'grade='.$grade;
 
-    if (mysqli_num_rows($result) > 0) {
-    	$row = mysqli_fetch_assoc($result);
-    }else {
-    	header("Location: ../teacher/teacher_view.php");
-    }
-
-
-}else if(isset($_POST['update'])){
-    include "db_conn.php";
-    function validate($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-	}
-        $lrnnumber = validate($_POST['lrnnumber']);
-	$firstname = validate($_POST['firstname']);
-	$middlename = validate($_POST['middlename']);
-        $lastname = validate($_POST['lastname']);
-        $gender = validate($_POST['gender']);
-        $birthplace = validate($_POST['birthplace']);
-        $birthday = validate($_POST['birthday']);
-        $suffix = validate($_POST['suffix']);
-        $age = validate($_POST['age']);
-        $address = validate($_POST['address']);
-        $parent = validate($_POST['parent']);
-    
-  
-        $schoolyear = validate($_POST['schoolyear']);
-        $grade = validate($_POST['grade']);
-        $section = validate($_POST['section']);
-
-        $id = validate($_POST['id']);
-
-        
-
-	if (empty($firstname)) {
-		header("Location: ../update.php?id=$id&error=First Name is required/Input new data");
-	}
-        
-        else if (empty($middlename)) {
-		header("Location: ../update.php?id=$id&error=Middle Name is required/Input new data");
-	}
-        else if (empty($lastname)) {
-		header("Location: ../update.php?id=$id&error=LRN number is required/Input new data");
-	}
-        
-        else if (empty($lrnnumber)) {
-		header("Location: ../update.php?id=$id&error=LRN number is required/Input new data");
+	if (empty($grade))
+	 {
+		header ("Location:../add.php?error=grade is required&$user_data");
 	}
 
-        else if (empty($gender)) {
-		header("Location: ../update.php?id=$id&error=Gender is required/Input new data");
-	}
-        
-        else if (empty($birthplace)) {
-		header("Location: ../update.php?id=$id&error=Birth Place is required/Input new data");
-	}
-        else if (empty($address)) {
-		header("Location: ../update.php?id=$id&error=Birth Place is required/Input new data");
-	}
-        else if (empty($parent)) {
-		header("Location: ../update.php?id=$id&error=Birth Place is required/Input new data");
-	}
-        
-        else if (empty($age)) {
-		header("Location: ../update.php?id=$id&error=Birth Place is required/Input new data");
-	}
-        else if (empty($schoolyear)) {
-		header("Location: ../update.php?id=$id&error=Birth Place is required/Input new data");
-	}
-        
-        else if (empty($section)) {
-		header("Location: ../update.php?id=$id&error=Birth Place is required/Input new data");
-	}
 
-        
-        
-        else {
+	
+	if (mysqli_num_rows($duplicate)>0)
+	{
+		header ("Location:../add.php?error=Student Already Got a Grade&$user_data");
+	}
+	
+	else {
 
-       $sql = "UPDATE students
-               SET lrnnumber='$lrnnumber',firstname='$firstname', middlename='$middlename',
-               lastname='$lastname',birthplace='$birthplace',
-               suffix='$suffix',gender='$gender',
-               birthday='$birthday',age='$age',
-               address='$address',parent='$parent',schoolyear='$schoolyear',grade='$grade',section='$section'
+       $sql = "INSERT INTO grade(subjectname,studentid,grade)
+               VALUES('$subjectname','$studentid','$grade')";
 
-               WHERE id=$id ";
        $result = mysqli_query($conn, $sql);
+
        if ($result) {
-       	  header("Location: ../teacher_read.php?id=$id&success=successfully updated");
-          
+		header("Location: ../add.php?success=Added Successfully");
+		 
        }
-       
-       else {
-          header("Location: ../update.php?id=$id&error=unknown error occurred&$user_data");
-       }
+	 
 	}
-}else {
-	header("Location: teacher_read.php");
+
+
+
 }
+
+
