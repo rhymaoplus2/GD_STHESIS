e85fd6d8c3750a16ae2368ec1ddf80db977f0e8a
Reyris Milmao <reyrismilmao@gmail.com>
2023-02-26 15:44:14 +0800

    buttons student admin view

--- Thesis/admin/php/logout.php
+++ Thesis/admin/php/logout.php
@@ -4,4 +4,4 @@ session_start();
 session_unset();
 session_destroy();
 
-header("Location: ../admin.php");
+header("Location: ../index.php");
--- Thesis/admin/students/teacher_read.php
+++ Thesis/admin/students/teacher_read.php
@@ -185,8 +185,8 @@ font-size: 10px;;
   }
 
   .btn img {
-    width: 20px;
-    height: 20px;
+    width: 30px;
+    height: 30px;
   }
   #refresh-img {
   transition: all 0.2s;
@@ -194,6 +194,9 @@ font-size: 10px;;
 
 #refresh-img:hover {
   transform: scale(1.2);
+}
+#search {
+  border-width: 2px;
 }
 
   </style>
@@ -228,43 +231,65 @@ font-size: 10px;;
 
 
         <div class="d-flex justify-content-center">
-  <a href="teacher_create.php" class="btn btn-dark mb-3"><b>ADD NEW </b></a>
+
 </div>
 <?php
-  require "./php/db_conn.php";
+require "./php/db_conn.php";
 
-  $comment = "";
-  if(isset($_POST['submit'])) {
-    $section = $_POST['section'];
-    $grade = $_POST['grade'];
-    $comment = "Showing results for ";
+$comment = "";
+$where_clause = "1";
 
-    if(!empty($section) && !empty($grade)) {
-      $comment .= "Grade $grade - Section $section";
-    } elseif(!empty($section)) {
-      $comment .= "Section $section";
-    } elseif(!empty($grade)) {
-      $comment .= "Grade $grade";
-    } else {
-      $comment .= "All";
-    }
+if(isset($_POST['submit'])) {
+  $section = $_POST['section'];
+  $grade = $_POST['grade'];
+  $trackstrand = $_POST['trackstrand'];
+  $comment = "Showing results for ";
+
+  if(!empty($section) && !empty($grade) && !empty($trackstrand)) {
+    $comment .= "Grade $grade - Section $section - Track/Strand $trackstrand";
+    $where_clause = "section = '$section' AND grade = '$grade' AND trackstrand = '$trackstrand'";
+  } elseif(!empty($section) && !empty($grade)) {
+    $comment .= "Grade $grade - Section $section";
+    $where_clause = "section = '$section' AND grade = '$grade'";
+  } elseif(!empty($section) && !empty($trackstrand)) {
+    $comment .= "Section $section - Track/Strand $trackstrand";
+    $where_clause = "section = '$section' AND trackstrand = '$trackstrand'";
+  } elseif(!empty($grade) && !empty($trackstrand)) {
+    $comment .= "Grade $grade - Track/Strand $trackstrand";
+    $where_clause = "grade = '$grade' AND trackstrand = '$trackstrand'";
+  } elseif(!empty($section)) {
+    $comment .= "Section $section";
+    $where_clause = "section = '$section'";
+  } elseif(!empty($grade)) {
+    $comment .= "Grade $grade";
+    $where_clause = "grade = '$grade'";
+  } elseif(!empty($trackstrand)) {
+    $comment .= "Track/Strand $trackstrand";
+    $where_clause = "trackstrand = '$trackstrand'";
+  } else {
+    $comment .= "All";
+    $where_clause = "1";
   }
+}
+
+
+$query = "SELECT * FROM students WHERE $where_clause";
+$result = mysqli_query($conn, $query);
 ?>
+
 <form method="post" class="text-center">
-<?php
+  <?php
     if(!empty($comment)) {
       echo "<p><b>$comment</b>  </p>";
     }
   ?>
-  <!--<label for="sort">Sort By:</label>
-  <select id="sort" name="sort">
-    <option value="lastname">Last Name</option>
-    <option value="firstname">First Name</option>
-    <option value="middlename">Middle Name</option>
-    <option value="lrnnumber">LRN No.</option>
-  </select>
-  &nbsp;&nbsp;&nbsp; 
-      --> 
+<a href="teacher_create.php" class="btn btn-transparent p-0 mb-3">
+  <b></b>
+  <img id="refresh-img" src="img/add.gif" alt="Image" title="Add New Student" width="30" height="auto">
+</a>
+
+
+  &nbsp;&nbsp;&nbsp;
   <label for="section">Section:</label>
   <select id="section" name="section">
     <option value="">All</option>
@@ -289,21 +314,43 @@ font-size: 10px;;
     <option value="11">11</option>
     <option value="12">12</option>
   </select>
+  &nbsp;&nbsp;&nbsp;
+
+  <label for="strand">Track/Strand:</label>
+  <select id="trackstrand" name="trackstrand">
+    <option value="">All</option>
+    <?php
+      require "./php/db_conn.php";
+      $query = "SELECT DISTINCT name FROM trackstrand";
+      $result = mysqli_query($conn, $query);
+
+      while($row = mysqli_fetch_array($result)) {
+        echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+      }
+    ?>
+  </select>
   &nbsp;&nbsp;&nbsp;  
-  <button class="btn btn-transparent p-0" type="submit" name="submit" >
-  <img id="refresh-img" src="img/refresh.png" alt="Image" width="50" height="50">
-</button>
-<script>
-var refreshImg = document.getElementById("refresh-img");
 
-refreshImg.addEventListener("click", function() {
-  refreshImg.style.transform = "scale(1.2) rotate(360deg)";
-  setTimeout(function() {
-    refreshImg.style.transform = "";
-  }, 2000);
-});
+  <button class="btn btn-transparent p-0" type="submit" name="submit" title="Sort">
+    <img id="refresh-img" src="img/refresh.png" alt="Image" width="50" height="50">
+  </button>
 
+  <script>
+    var refreshImg = document.getElementById("refresh-img");
+
+    refreshImg.addEventListener("click", function() {
+      refreshImg.style.transform = "scale(1.2) rotate(360deg)";
+      setTimeout(function() {
+        refreshImg.style.transform = "";
+      }, 2000);
+    });
   </script>
+  &nbsp;&nbsp;&nbsp;
+  <input type="text" name="search" placeholder="Search...">
+  &nbsp;&nbsp;
+  <button type="submit" class="btn btn-transparent p-0">
+  <img id="refresh-img" src="img/search.gif" alt="Image" title="Search" width="30" height="30">
+</button>
 
 </form>
 
@@ -316,58 +363,69 @@ refreshImg.addEventListener("click", function() {
       <th scope="col">First Name</th>
       <th class="text-center" scope="col">M.I.</th>
       <th class="text-center" scope="col">LRN No.</th>
-      <th class="text-center" scope="col">Section</th>
+      <th hidden class="text-center" scope="col">Section</th>
       <th scope="col" colspan="3" class="text-center">Actions</th>
     </tr>
   </thead>
   <tbody>
 
-<?php
+  <?php
  require "./php/db_conn.php";
 
- $query = "SELECT * FROM students";
- $result = mysqli_query($conn, $query);
+ if (isset($_SESSION['username']) && isset($_SESSION['id'])) {
+  $section = isset($_POST['section']) ? $_POST['section'] : '';
+  $grade = isset($_POST['grade']) ? $_POST['grade'] : '';
+  $trackstrand = isset($_POST['trackstrand']) ? $_POST['trackstrand'] : '';
+  $search = isset($_POST['search']) ? $_POST['search'] : '';
+  
+  $where_clause = '';
+  if (!empty($section)) {
+    $where_clause .= "section = '$section'";
+  }
+  if (!empty($grade)) {
+    if (!empty($where_clause)) {
+      $where_clause .= ' AND ';
+    }
+    $where_clause .= "grade = '$grade'";
+  }
+  
+  if (!empty($trackstrand)) {
+    if (!empty($where_clause)) {
+      $where_clause .= ' AND ';
+    }
+    $where_clause .= "trackstrand = '$trackstrand'";
+  }
 
- if (mysqli_num_rows($result) > 0) {
-   if (isset($_SESSION['username']) && isset($_SESSION['id'])) {
-     $sort_by = isset($_POST['sort']) ? $_POST['sort'] : 'lastname';
-     $section = isset($_POST['section']) ? $_POST['section'] : '';
-     $grade = isset($_POST['grade']) ? $_POST['grade'] : '';
+  if (!empty($search)) {
+    if (!empty($where_clause)) {
+      $where_clause .= ' AND ';
+    }
+    $where_clause .= "(lastname LIKE '%$search%' OR firstname LIKE '%$search%' OR middlename LIKE '%$search%' OR section LIKE '%$search%' OR trackstrand LIKE '%$search%' OR lrnnumber LIKE '%$search%' OR id LIKE '%$search%')";
+  }
 
-     $where_clause = '';
-     if (!empty($section)) {
-       $where_clause .= "section = '$section'";
-     }
-     if (!empty($grade)) {
-       if (!empty($where_clause)) {
-         $where_clause .= ' AND ';
-       }
-       $where_clause .= "grade = '$grade'";
-     }
+  if (!empty($where_clause)) {
+    $where_clause = "WHERE $where_clause";
+  }
 
-     if (!empty($where_clause)) {
-       $where_clause = "WHERE $where_clause";
-     }
+  $query = "SELECT * FROM students $where_clause ORDER BY lastname";
+  $result = mysqli_query($conn, $query);
 
-     $query = "SELECT * FROM students $where_clause ORDER BY $sort_by";
-     $result = mysqli_query($conn, $query);
-   }
-
-   while ($Row = mysqli_fetch_assoc($result)) {
+  while ($Row = mysqli_fetch_assoc($result)) {
 ?>
-      
-     
+
            <tr>
            <td hidden><?php echo $Row["id"]; ?></td>
            <td><?php echo $Row["lastname"]; ?></td>
           <td><?php echo $Row["firstname"]; ?></td>
           <td class="text-center"><?php echo substr($Row["middlename"], 0, 1); ?>.</td>
           <td class="text-center"><?php echo $Row["lrnnumber"]; ?></td>
-          <td class="text-center"><?php echo $Row["section"]; ?></td>
+          <td hidden class="text-center"><?php echo $Row["section"]; ?></td>
           <td hidden><?php echo $Row["grade"]; ?></td>
-         
+          <td hidden><?php echo $Row["trackstrand"]; ?></td>
   
-     <td class="text-center"><a href="view.php?id=<?=$Row['id']?>" 
+     <td class="text-center">
+      
+     <a href="view.php?id=<?=$Row['id']?>" 
 			      	     class="btn btn-dark "><b>VIEW</b></a>
                    
               <a href="update.php?id=<?=$Row['id']?>" 
@@ -424,10 +482,11 @@ refreshImg.addEventListener("click", function() {
 
 
 
+      }
+    }
 
-}
 
-}
+
  ?>
 
 
