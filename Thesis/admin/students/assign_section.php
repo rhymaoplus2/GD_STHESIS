37994aac3c31b71811a3d527d355859a8dd7240b
Reyris Milmao <reyrismilmao@gmail.com>
2023-02-28 20:36:02 +0800

    section assigned to

---
+++ Thesis/admin/students/assign_section.php
@@ -0,0 +1,59 @@
+<?php
+require "./php/db_conn.php";
+
+$section_name = $_GET['section_name'];
+$user_name = $_GET['user_name'];
+
+// Check if the section is already assigned to the user in any of the tables
+$query = "SELECT * FROM section WHERE name='$section_name' AND (t1='$user_name' OR t2='$user_name' OR t3='$user_name' OR t4='$user_name' OR t5='$user_name' OR t6='$user_name' OR t7='$user_name' OR t8='$user_name' OR t9='$user_name' OR t10='$user_name')";
+$result = mysqli_query($conn, $query);
+$Row = mysqli_fetch_assoc($result);
+
+$query = "SELECT * FROM users WHERE name='$user_name' AND (sec1='$section_name' OR sec2='$section_name' OR sec3='$section_name' OR sec4='$section_name' OR sec5='$section_name' OR sec6='$section_name' OR sec7='$section_name' OR sec8='$section_name' OR sec9='$section_name' OR sec10='$section_name')";
+$result = mysqli_query($conn, $query);
+$Row2 = mysqli_fetch_assoc($result);
+
+$query = "SELECT * FROM students WHERE section='$section_name' AND (subjectteacher1='$user_name' OR subjectteacher2='$user_name' OR subjectteacher3='$user_name' OR subjectteacher4='$user_name' OR subjectteacher5='$user_name' OR subjectteacher6='$user_name' OR subjectteacher7='$user_name' OR subjectteacher8='$user_name' OR subjectteacher9='$user_name' OR subjectteacher10='$user_name')";
+$result = mysqli_query($conn, $query);
+$Row3 = mysqli_fetch_assoc($result);
+
+if ($Row || $Row2 || $Row3) {
+  // User is already assigned to the section in one of the tables
+  header("Location:sections.php?id=$id&error=User is already assigned to the section in one of the tables.");
+
+} else {
+  // Assign the section to the user
+  $assigned = false;
+  for ($i = 1; $i <= 10; $i++) {
+    $query = "SELECT t$i FROM section WHERE name='$section_name'";
+    $result = mysqli_query($conn, $query);
+    $Row = mysqli_fetch_assoc($result);
+
+    if ($Row["t$i"] == "") {
+      $query = "UPDATE section SET t$i='$user_name' WHERE name='$section_name'";
+      $result = mysqli_query($conn, $query);
+      
+      // Store the section name to the user's column
+      $sec_column_name = "sec$i";
+      $query = "UPDATE users SET $sec_column_name='$section_name' WHERE name='$user_name'";
+      $result = mysqli_query($conn, $query);
+
+      // Update the students table
+      $query = "UPDATE students SET subjectteacher$i='$user_name' WHERE section='$section_name'";
+      $result = mysqli_query($conn, $query);
+
+      $assigned = true;
+      break;
+    }
+  }
+  if ($assigned) {
+    header("Location:sections.php?id=$id&success=Section assigned to $user_name");
+ 
+  } else {
+    header("Location:sections.php?id=$id&error=All slots are occupied.");
+
+  }
+}
+
+mysqli_close($conn);
+?>
---
+++ Thesis/admin/students/sections.php
@@ -0,0 +1,332 @@
+<?php 
+   include "./php/read.php";
+   session_start();
+   include "./php/db_conn.php";
+
+   if (isset($_SESSION['username']) && isset($_SESSION['id'])) 
+                                                          {   ?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head><title>Section List</title>
+  <link  href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+  <header>
+
+</header>
+
+<style>
+  
+
+
+  .container {
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
+
+.container form {
+	width: auto;
+	padding: 20px;
+	border-radius: 30px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.box {
+	width: auto;
+}
+.container table {
+	padding: 20px;
+	border-radius: 10px;
+	
+  border:10px;
+  background-color: white;
+}
+.border {
+	padding: 20px;
+	border-radius: 10px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border:10px;
+  border-radius: 30px;
+}
+
+
+.link-right {
+	display: flex;
+	justify-content: flex-end;
+}
+
+
+.link-center {
+	display: flex;
+	justify-content: flex-end;
+}
+
+
+
+
+
+
+.thead
+{
+font-size: 10px;;
+
+}
+
+
+
+
+
+
+
+
+  .nav-item
+  {
+  
+      color:black;
+  
+  }
+  .subjectlist{
+  
+      margin-left: 5rem;
+      margin-top: 5rem;
+  }
+  
+  .studentlist{
+  
+  margin-left: 20rem;
+  margin-top: 5rem;
+  }
+  
+  
+  .button{
+  
+      margin-left: 5rem;
+      margin-top: 11rem;
+  }
+  
+  
+  .button1{
+  
+  margin-left: 5rem;
+  margin-top: 9.5rem;
+  }
+  
+  .title{
+      margin-left: 40rem;
+      margin-top: 1rem;
+      font-size: 3.5rem;
+  }
+  .text1
+  {
+      margin-left: 23rem;
+      margin-top: -20rem;
+      width: 45rem;
+      height: 10rem;
+  }
+  
+  .text2
+  {
+      margin-left: 23rem;
+      margin-top: -20rem;
+      width: 45rem;
+      height: 10rem;
+  }
+
+
+
+
+.top-container {
+    background-color: #f1f1f1;
+    padding: 30px;
+    text-align: center;
+  }
+  
+  .header {
+   
+    background-color: white;
+    color: #f1f1f1;
+  }
+  
+  .content {
+    padding: 16px;
+  }
+  
+  .sticky {
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+  
+  .sticky + .content {
+    padding-top: 102px;
+  }
+  .btn-transparent {
+    background-color: transparent;
+    border: none;
+  }
+
+  .btn-transparent:hover {
+    background-color: transparent;
+    border: none;
+  }
+
+  .btn-transparent:focus {
+    background-color: transparent;
+    border: none;
+    box-shadow: none;
+  }
+
+  .btn img {
+    width: 30px;
+    height: 30px;
+  }
+  #refresh-img {
+  transition: all 0.2s;
+}
+
+#refresh-img:hover {
+  transform: scale(1.2);
+}
+#search {
+  border-width: 2px;
+}
+
+  </style>
+</head>
+
+<body>
+
+
+<div class="header" id="myHeader">
+<?PHP include_once('header.php');?>
+</div>
+
+<div class="container" >
+		<div class="box">
+    <div class="content">
+
+    <?php if (isset($_GET['error'])) { ?>
+		   <div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+		    </div>
+		   <?php } ?>
+       </div>
+       <?php if (isset($_GET['success'])) { ?>
+           <div class="alert alert-success" role="alert">
+			  <?php echo $_GET['success']; ?>
+		    </div>
+		    <?php } ?>
+			<?php if (mysqli_num_rows($result)) { ?>
+        
+        <div class="border">
+       
+
+
+        <div class="d-flex justify-content-center">
+
+
+
+
+
+
+
+
+
+
+        <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Assign To</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  <?php
+    require "./php/db_conn.php";
+
+    $query = "SELECT name FROM section";
+    $result = mysqli_query($conn, $query);
+
+    while ($Row = mysqli_fetch_assoc($result)) {
+  ?>
+
+      <tr>
+        <td><?php echo $Row["name"]; ?></td>
+        <td>
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="assignToDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+              Assign To
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="assignToDropdown">
+              <?php
+                $query2 = "SELECT name FROM users";
+                $result2 = mysqli_query($conn, $query2);
+
+                while ($Row2 = mysqli_fetch_assoc($result2)) {
+              ?>
+                  <li><a class="dropdown-item" href="assign_section.php?section_name=<?php echo $Row['name']; ?>&user_name=<?php echo $Row2['name']; ?>"><?php echo $Row2["name"]; ?></a></li>
+              <?php
+                }
+              ?>
+            </ul>
+          </div>
+        </td>
+      </tr>
+
+  <?php
+    }
+  }
+}
+  ?>
+
+  </tbody>
+</table>
+
+
+
+</div>
+
+
+
+
+
+</div>
+
+
+
+<br>
+<br>
+
+
+
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+</script>
+
+</script>
+</body>
+</html>
+<?php 
+
+  
+                                                          
